d6972f0b604a16c85aedb12fcf9387bc2037155c
sourabh <sourabhlachheta@gmail.com>
2026-02-09 00:35:20 +0530

    adding frame on addroom class

--- src/Hotel/Management/System/AddRoom.java
+++ src/Hotel/Management/System/AddRoom.java
@@ -1,7 +1,15 @@
 package Hotel.Management.System;
 
-public class AddRoom {
+import javax.swing.*;
+
+public class AddRoom extends JFrame {
     AddRoom(){
+
+        setLayout(null);
+        setLocation(100,80);
+        setSize(1700,900);
+        setVisible(true);
+
 
     }
     public static void main(String[] args){

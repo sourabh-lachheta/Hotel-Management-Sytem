730f4feb8322b439075769c756fcc6649c19889a
sourabh <sourabhlachheta@gmail.com>
2026-02-11 23:36:52 +0530

    set up for adddriver class

---
+++ src/Hotel/Management/System/AddDriver.java
@@ -0,0 +1,16 @@
+package Hotel.Management.System;
+
+import javax.swing.*;
+
+public class AddDriver extends JFrame {
+
+    AddDriver(){
+        setLayout(null);
+        setLocation(100,80);
+        setSize(1700,900);
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        new AddDriver();
+    }
+}

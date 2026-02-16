b11c274571878086763b1675d36be433b9570ca1
sourabh <sourabhlachheta@gmail.com>
2026-02-16 23:51:19 +0530

    setup for employee class

---
+++ src/Hotel/Management/System/Employee.java
@@ -0,0 +1,17 @@
+package Hotel.Management.System;
+
+import javax.swing.*;
+
+public class Employee extends JFrame {
+
+    Employee(){
+        setLayout(null);
+        setLocation(700,150);
+        setSize(1020,700);
+        setVisible(true);
+
+    }
+    public static void main(String[] args){
+        new Employee();
+    }
+}

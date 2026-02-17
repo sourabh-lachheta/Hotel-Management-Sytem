2f076aec4f524303584971dced15c8238af3b979
sourabh <sourabhlachheta@gmail.com>
2026-02-17 19:22:36 +0530

    complete employee frame and adding data on it

--- src/Hotel/Management/System/Employee.java
+++ src/Hotel/Management/System/Employee.java
@@ -1,16 +1,114 @@
 package Hotel.Management.System;
 
-import javax.swing.*;
+import net.proteanit.sql.DbUtils;
 
-public class Employee extends JFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+
+public class Employee extends JFrame implements ActionListener {
+    JTable table;
+    JButton b1;
+    JLabel name,age,gender,job,salary,phone,aadhar,email;
 
     Employee(){
+
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBounds(5,5,1010,690);
+        panel.setBackground(new Color(155,135,66,85));
+        add(panel);
+
+        b1 = new JButton("back");
+        b1.setBounds(420,600,100,30);
+        b1.addActionListener(this);
+        panel.add(b1);
+
+        name = new JLabel("Name");
+        name.setBounds(40,15,50,30);
+        name.setFont(new Font("GC Omega",Font.BOLD,16));
+        panel.add(name);
+
+        age = new JLabel("Age");
+        age.setBounds(150,15,50,30);
+        age.setFont(new Font("GC Omega",Font.BOLD,16));
+        panel.add(age);
+
+        gender = new JLabel("Gender");
+        gender.setBounds(260,15,80,30);
+        gender.setFont(new Font("GC Omega",Font.BOLD,16));
+        panel.add(gender);
+
+        job = new JLabel("Job");
+        job.setBounds(380,15,80,30);
+        job.setFont(new Font("GC Omega",Font.BOLD,16));
+        panel.add(job);
+
+        salary = new JLabel("Salary");
+        salary.setBounds(490,15,80,30);
+        salary.setFont(new Font("GC Omega",Font.BOLD,16));
+        panel.add(salary);
+
+        phone = new JLabel("Phone");
+        phone.setBounds(600,15,80,30);
+        phone.setFont(new Font("GC Omega",Font.BOLD,16));
+        panel.add(phone);
+
+        aadhar = new JLabel("Aadhar");
+        aadhar.setBounds(710,15,80,30);
+        aadhar.setFont(new Font("GC Omega",Font.BOLD,16));
+        panel.add(aadhar);
+
+        email = new JLabel("Email");
+        email.setBounds(820,15,80,30);
+        email.setFont(new Font("GC Omega",Font.BOLD,16));
+        panel.add(email);
+
+
+
+
+
+
+        table = new JTable();
+        table.setBounds(30,40,900,600);
+        table.setBackground(new Color(155,135,66,85));
+        table.setFont(new Font("GC Omega",Font.BOLD,12));
+        panel.add(table);
+
+        try{
+            Temp c = new Temp();
+            String empInfo = "select * from employee";
+            ResultSet resultset = c.statement.executeQuery(empInfo);
+            table.setModel(DbUtils.resultSetToTableModel(resultset));
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+       // setUndecorated(true);
         setLayout(null);
         setLocation(700,150);
         setSize(1020,700);
         setVisible(true);
 
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == b1){
+            setVisible(false);
+        }
+    }
+
     public static void main(String[] args){
         new Employee();
     }
--- src/Hotel/Management/System/Reception.java
+++ src/Hotel/Management/System/Reception.java
@@ -51,6 +51,7 @@ public class Reception extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e){
                 try{
+                    new Employee();
 
                 }catch(Exception E){
                     E.printStackTrace();
